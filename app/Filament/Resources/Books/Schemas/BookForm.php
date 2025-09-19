991d10db6b7608f627af20372333af84358d5ba4
mickey <mickeyjiyestha@gmail.com>
2025-09-19 21:21:09 +0800

    adding book CRUD features

--- app/Filament/Resources/Books/Schemas/BookForm.php
+++ app/Filament/Resources/Books/Schemas/BookForm.php
@@ -3,14 +3,15 @@
 namespace App\Filament\Resources\Books\Schemas;
 
 use Filament\Schemas\Schema;
-
+use Filament\Forms\Components\TextInput;
 class BookForm
 {
     public static function configure(Schema $schema): Schema
     {
         return $schema
             ->components([
-                //
+                Textinput::make('title')->label('Title')->required(),
+                Textinput::make('quantity')->label('Quantity')->required(),
             ]);
     }
 }
--- app/Filament/Resources/Books/Tables/BooksTable.php
+++ app/Filament/Resources/Books/Tables/BooksTable.php
@@ -6,6 +6,7 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
 
 class BooksTable
 {
@@ -13,7 +14,8 @@ class BooksTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('title')->label('Title'),
+                TextColumn::make('quantity')->label('Quantity'),
             ])
             ->filters([
                 //
